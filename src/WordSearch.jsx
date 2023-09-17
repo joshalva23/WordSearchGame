3670516d5c6bfa89b3cbd0c17f4bc3db29cf7948
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2023-09-17 10:38:07 +0530

    Added WordList

--- src/WordSearch.jsx
+++ src/WordSearch.jsx
@@ -205,6 +205,7 @@ function WordListTab({genWordList}){
   );
 }
 
+
 export default function WordSearch() {
   const [genWordList, setGenWordList] = useState([]);
   const [_Crossword, setCrossword] = useState([]);
@@ -254,7 +255,7 @@ export default function WordSearch() {
       });
       const wordelement = document?.getElementById(`wordname${genWordList.indexOf(WordList)}`);
       wordelement.getElementsByTagName('p')[0].innerHTML= `${WordList}✅`;
-      wordelement.scrollIntoView({behaviour:'smooth'});
+      wordelement.scrollIntoViewIfNeeded({behaviour:'smooth'});
       setSelectedWords([]);
       setWordList('');
       setColorSelector((colorselector+1)%colors.length);
@@ -317,7 +318,6 @@ export default function WordSearch() {
             <span><p className = "remCount" id="remCount">{remainingWords}</p></span>
           </div>
           <WordListTab genWordList={genWordList}/>
-          
         </div>
         <Board crossword={_Crossword} onSquareClick={handleClick} />
       </div>
