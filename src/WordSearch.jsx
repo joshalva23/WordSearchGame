bcabdcb882b15294247d7f93c342ec8d511c7495
joshalva23 <joshuaelias.cs21@rvce.edu.in>
2024-07-06 12:26:42 +0530

    Recent

--- src/WordSearch.jsx
+++ src/WordSearch.jsx
@@ -61,7 +61,7 @@ class Crossword {
 
       index--;
     }
-    this.printCrossword();
+    //this.printCrossword();
 
     for (let i = 0; i < gridSize; i++) {
       for (let j = 0; j < gridSize; j++) {
